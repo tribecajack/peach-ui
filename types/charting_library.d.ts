db2825025d58104ed8beb44d91fef1c525f92e25
penrose <penrose@ultra.markets>
2025-06-26 21:30:32 -0400

    fix: Correctly configure charting_library types

---
+++ types/charting_library.d.ts
@@ -0,0 +1,19 @@
+declare module '@public/charting_library' {
+  export type ResolutionString = string;
+  export type SearchSymbolResultItem = any;
+  export type HistoryMetadata = any;
+  export type PeriodParams = any;
+  export type OnReadyCallback = (configuration: DatafeedConfiguration) => void;
+  export type IDatafeedChartApi = any;
+  export type IExternalDatafeed = any;
+  export type DatafeedConfiguration = any;
+  export type LibrarySymbolInfo = any;
+  export type Bar = any;
+  export const widget: any;
+  export type ChartingLibraryWidgetOptions = any;
+  export type IChartingLibraryWidget = any;
+  export type IOrderLineAdapter = any;
+  export type AvailableSaveloadVersions = any;
+  export type IExecutionLineAdapter = any;
+  export type Direction = any;
+} 
